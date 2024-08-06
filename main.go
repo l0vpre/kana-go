f2f0ecc71e6f3e12d76c9353316e9b8052d972fa
l0vpre <l0vpre@tutamail.com>
2024-08-07 00:08:16 +0700

    5 and 6 task comleted

--- main.go
+++ main.go
@@ -5,11 +5,21 @@ import (
     "math/rand"
 )
 
+type Stats struct {
+    Answer int
+    CorrectAnswer int
+    WrongAnswer map[string]bool
+}
+
+func(stats Stats) StatsToString()string{
+   return  fmt.Sprintf("\nAnswer: %d \nCorrect Answer: %d\n",
+                        stats.Answer,stats.CorrectAnswer)
+}
+
 func randKey(kana map[string]string)string{
     randKey := rand.Intn(len(kana))
     i := 0
-    for key:= range kana{
-
+    for key := range kana{
         if (i == randKey){
             return key
         }
@@ -17,7 +27,9 @@ func randKey(kana map[string]string)string{
     }
     return "no"
 }
+
 func main(){
+    var answer string
     kana := map[string]string{
         "a": "あ",
         "i": "い",
@@ -25,17 +37,37 @@ func main(){
         "e": "え",
         "o": "お",
     }
-    
-    key := randKey(kana)
-    fmt.Println(kana[key])
-    var answer string
-    fmt.Scan(&answer)
-    if(answer == key){
-        fmt.Println("yes")
-    } else {
-        fmt.Println("no, this: ", key)
+    stats := Stats{
+        Answer: 0,
+        CorrectAnswer: 0,
+        WrongAnswer: make(map[string]bool),
     }
 
-    fmt.Print(answer)
+    for{
+        key := randKey(kana)
+        fmt.Println("What this kana: ", kana[key], "?")
+        fmt.Scan(&answer)
 
-}
+        if(answer == "exit"){
+            fmt.Print("Wrong kana:\n")
+            for key:= range stats.WrongAnswer{
+                fmt.Println(key, " ", kana[key])
+            }
+            return
+        }
+
+        if(answer == key){
+            fmt.Println("yes")
+            stats.CorrectAnswer++
+        } else {
+            fmt.Println("no, this: ", key)
+            stats.WrongAnswer[key] = true
+        }
+
+        stats.Answer++
+        fmt.Println(stats.StatsToString())
+    }
+}   
+
+
+
